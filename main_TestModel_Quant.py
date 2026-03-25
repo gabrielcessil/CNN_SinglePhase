ceefd16cf75e12189df40a51ccaf40e1719d78f8
gabrielcessil <gabrielcessil@gmail.com>
2026-03-25 16:35:29 -0300

    Adjusting Quantitative Analysis

--- main_TestModel_Quant.py
+++ main_TestModel_Quant.py
@@ -13,79 +13,7 @@ from matplotlib.ticker import LogLocator, LogFormatterSciNotation
 from Utilities import dataset_reader as dr
 from torch.utils.data import DataLoader
 from Utilities import nn_trainner as nnt
-
-
-#######################################################
-#************ BASELINE MODEL:              ***********#
-#######################################################
-
-# Encapsulate Danny Ko. model
-class Danny_KerasModel(nn.Module):
-    def __init__(self, uni_directional=None):
-        super().__init__() 
-        self.uni_directional = uni_directional
-        if uni_directional == 0:
-            eval_model_name     = 'UnetRS_ModelvZ1-4'
-            path = "../Run_Keras/"+"RandomSphere SubModel/{}/{}.ckpt".format(eval_model_name, eval_model_name)
-            self.model          = tf.keras.models.load_model(
-                 path
-            )
-        elif uni_directional == 1:
-            eval_model_name     = 'UnetRS_ModelvY1-1'
-            path = "../Run_Keras/"+"RandomSphere SubModel/{}/{}.ckpt".format(eval_model_name, eval_model_name)
-            self.model          = tf.keras.models.load_model(
-                 path
-            )
-        elif uni_directional == 2:
-            eval_model_name     = 'UnetRS_ModelvX1-3'
-            path = "../Run_Keras/"+"RandomSphere SubModel/{}/{}.ckpt".format(eval_model_name, eval_model_name)
-            self.model          = tf.keras.models.load_model(
-                 path
-            )
-        else:
-            eval_model_name     = 'UnetRSXYZ_ModelvXYZ1-8'
-            path = "../Run_Keras/"+"RandomSphere FinalModel/{}/{}.ckpt".format(eval_model_name, eval_model_name)
-            self.model          = tf.keras.models.load_model(
-                 path,
-                 custom_objects = {'div_loss2': Danny_KerasModel.div_loss2}
-            )
-        
-    # Inputs: Tensor (B,1,Z=120,Y=120,X=120),where 0's are solids, 1's are pores
-    def predict(self, inputs):
-        # Pytorch (B,C,Z,Y,X) -> Tensorflow (B,Z,Y,X,C)
-        x          = inputs.permute(0,2,3,4,1)   
-        # Use it as boolean numpy
-        x          = x.detach().cpu().numpy()        
-        # Danny works alligned to X
-        x          = np.transpose(x, axes=[0, 3, 2, 1, 4])
-        # Make sure it is binary
-        x          = (x>0).astype(np.float32)
-        # Predict
-        y          = np.float32(self.model.predict(x=[x]))
-        # Rotate alligning to Z
-        y          = np.transpose(y, axes=[0, 3, 2, 1, 4])
-        # Return prediction as Pytorch tensor
-        y          = torch.from_numpy(y)
-        # Tensorflow (B,Z,Y,X,C) -> Pytorch (B,C,Z,Y,X)
-        y          = y.permute(0,4,1,2,3)
-        # Reorder the channels: C=Vx,Vy,Vz to C=Vz,Vy,Vx
-        if self.uni_directional is None:
-            y          = y[:, [2, 1, 0], :, :, :]
-        return y
- 
-    @staticmethod
-    def div_loss2(y_true, y_pred):
-
-        scale         = 3
-        mse           = tf.math.reduce_mean( tf.math.square(y_true - y_pred) )
-        dVxdx_pred    = (y_pred[:,2:,1:-1,1:-1,0] - y_pred[:,:-2,1:-1,1:-1,0])/2
-        dVydy_pred    = (y_pred[:,1:-1,2:,1:-1,1] - y_pred[:,1:-1,:-2,1:-1,1])/2
-        dVzdz_pred    = (y_pred[:,1:-1,1:-1,2:,2] - y_pred[:,1:-1,1:-1,:-2,2])/2
-        div_pred      = dVxdx_pred + dVydy_pred + dVzdz_pred
-        div_loss      = tf.math.reduce_mean( tf.math.abs(div_pred) )
-        loss          = mse + div_loss*scale
-
-        return loss
+from Danny_Original.architecture import Danny_KerasModel
 
 
 #######################################################
@@ -543,22 +471,22 @@ device           = 'cpu'
 datasets        = {
     #"Trainning": "../NN_Datasets/PressureDriven/Train_Danny_120_120_120_PressureWalls.h5",
     
-    "Spherical Pore":   "../NN_Datasets/ForceDriven/Test_SphPore_120_120_120.h5",
-    "Spherical Grain":  "../NN_Datasets/ForceDriven/Test_SphGrain_120_120_120.h5",
-    "Cylindrical Pore": "../NN_Datasets/ForceDriven/Test_CylinPore_120_120_120.h5",
-    "Cylindrical Grain":"../NN_Datasets/ForceDriven/Test_CylinGrain_120_120_120.h5",
+    #"Spherical Pore":   "../NN_Datasets/ForceDriven/Test_SphPore_120_120_120.h5",
+    #"Spherical Grain":  "../NN_Datasets/ForceDriven/Test_SphGrain_120_120_120.h5",
+    #"Cylindrical Pore": "../NN_Datasets/ForceDriven/Test_CylinPore_120_120_120.h5",
+    #"Cylindrical Grain":"../NN_Datasets/ForceDriven/Test_CylinGrain_120_120_120.h5",
     
     "Parker":       "../NN_Datasets/ForceDriven/Test_Oliveira_Parker_120_120_120.h5",
-    "Leopard":      "../NN_Datasets/ForceDriven/Test_Oliveira_Leopard_120_120_120.h5",
-    "Kirby":        "../NN_Datasets/ForceDriven/Test_Oliveira_Kirby_120_120_120.h5",
-    "Castle Gate":  "../NN_Datasets/ForceDriven/Test_Oliveira_CastleGate_120_120_120.h5",
-    "Brown":        "../NN_Datasets/ForceDriven/Test_Oliveira_Brown_120_120_120.h5",
-    "Upper Gray":   "../NN_Datasets/ForceDriven/Test_Oliveira_BereaUpperGray_120_120_120.h5",
-    "Sinter Gray":  "../NN_Datasets/ForceDriven/Test_Oliveira_BereaSinterGray_120_120_120.h5",
-    "Berea Buff":   "../NN_Datasets/ForceDriven/Test_Oliveira_BereaBuff_120_120_120.h5",
-    "Berea":        "../NN_Datasets/ForceDriven/Test_Oliveira_Berea_120_120_120.h5",
-    "Bentheimer":   "../NN_Datasets/ForceDriven/Test_Oliveira_Bentheimer_120_120_120.h5",
-    "Bandera":      "../NN_Datasets/ForceDriven/Test_Oliveira_Bandera_120_120_120.h5",
+    #"Leopard":      "../NN_Datasets/ForceDriven/Test_Oliveira_Leopard_120_120_120.h5",
+    #"Kirby":        "../NN_Datasets/ForceDriven/Test_Oliveira_Kirby_120_120_120.h5",
+    #"Castle Gate":  "../NN_Datasets/ForceDriven/Test_Oliveira_CastleGate_120_120_120.h5",
+    #"Brown":        "../NN_Datasets/ForceDriven/Test_Oliveira_Brown_120_120_120.h5",
+    #"Upper Gray":   "../NN_Datasets/ForceDriven/Test_Oliveira_BereaUpperGray_120_120_120.h5",
+    #"Sinter Gray":  "../NN_Datasets/ForceDriven/Test_Oliveira_BereaSinterGray_120_120_120.h5",
+    #"Berea Buff":   "../NN_Datasets/ForceDriven/Test_Oliveira_BereaBuff_120_120_120.h5",
+    #"Berea":        "../NN_Datasets/ForceDriven/Test_Oliveira_Berea_120_120_120.h5",
+    #"Bentheimer":   "../NN_Datasets/ForceDriven/Test_Oliveira_Bentheimer_120_120_120.h5",
+    #"Bandera":      "../NN_Datasets/ForceDriven/Test_Oliveira_Bandera_120_120_120.h5",
     }
 
 
@@ -568,10 +496,10 @@ models          = {}
 if directional==0:
     
     # Baseline model
-    #print("\nLoading Danny Ko (Baseline)...")
-    #baseline_model  = Danny_KerasModel(uni_directional=0)
-    #print_n_params(baseline_model.model, pytorch=False)
-    #models["Baseline Danny (Ke) - Danny Data"] = baseline_model
+    print("\nLoading Danny Ko (Baseline)...")
+    baseline_model  = Danny_KerasModel(uni_directional=0)
+    print_n_params(baseline_model.model, pytorch=False)
+    models["Baseline Danny (Ke) - Danny Data"] = baseline_model
     
 
     # DATASETS COMPARISON
